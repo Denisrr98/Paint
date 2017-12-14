56c79f8510b691eb8721f5b1b24fa8fd8f38b67b
Devoloper account <dev@minimal.localdomain>
2017-12-14 18:26:32 +0300

    Fixed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,88 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QInputDialog>
+#include <QColorDialog>
+#include <QDesktopWidget>
+#include <QMouseEvent>
+#include <QPaintEvent>
+#include <QPainter>
+#include <QImage>
+
+#define DEFAULT_SIZE 5
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    mImage = new QImage(QApplication::desktop()->size(),
+                        QImage::Format_ARGB32_Premultiplied);
+    mPainter = new QPainter(mImage);
+    mEnabled = false;
+    mColor = QColor(Qt::black);
+    mSize = DEFAULT_SIZE;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete mPainter;
+    delete mImage;
 }
+
+void MainWindow::paintEvent(QPaintEvent *e)
+{
+    QPainter painter(this);
+    painter.fillRect(mImage->rect(), Qt::white);
+    painter.drawImage(0, 0, *mImage);
+    e->accept();
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *e)
+{
+    mEnabled = true;
+    mBegin = e->pos();
+    e->accept();
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *e)
+{
+    if (!mEnabled)
+    {
+        e->accept();
+        return;
+    }
+    QPen pen(mColor);
+    pen.setCapStyle(Qt::RoundCap);
+    pen.setWidth(mSize);
+    mEnd = e->pos();
+    mPainter->setPen(pen);
+    mPainter->drawLine(mBegin, mEnd);
+    mBegin = mEnd;
+    update();
+    e->accept();
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *e)
+{
+    mEnabled = false;
+    e->accept();
+}
+
+void MainWindow::on_actionThickness_triggered()
+{
+    mSize = QInputDialog::getInt(this, "Size of pen", "EnterSize:", 5, 1);
+}
+
+void MainWindow::on_actionColor_triggered()
+{
+    mColor = QColorDialog::getColor(Qt::black, this, "Color of pen");
+}
+
+void MainWindow::on_actionExit_without_saving_triggered()
+{
+    close();
+}
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -7,16 +7,35 @@ namespace Ui {
 class MainWindow;
 }
 
+class QPainter;
+class QImage;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+protected:
+    void paintEvent(QPaintEvent *e) override;
+    void mousePressEvent(QMouseEvent *e) override;
+    void mouseMoveEvent(QMouseEvent *e) override;
+    void mouseReleaseEvent(QMouseEvent *e) override;
+private slots:
+    void on_actionThickness_triggered();
+    void on_actionColor_triggered();
+    void on_actionExit_without_saving_triggered();
+    void on_actionSize_triggered();
 
 private:
     Ui::MainWindow *ui;
+    QPainter *mPainter;     // paint
+    QImage *mImage;         // image
+    QPoint mBegin;          // start
+    QPoint mEnd;            // final
+    QColor mColor;          // color
+    bool mEnabled;          // activation
+    int mSize;              // size
 };
 
 #endif // MAINWINDOW_H
